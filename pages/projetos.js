3b136f3585f61d939317a13318b78f12cbd2d1ed
ArthurBrigueli <arthurbriguelifort@gmail.com>
2023-05-16 16:52:17 -0300

    first commit

---
+++ pages/projetos.js
@@ -0,0 +1,44 @@
+const container_conteudo = document.querySelector('.container-conteudo')
+const mais = document.querySelector('.mais')
+
+const getRepositorio = async() => {
+    const apiGithub = `https://api.github.com/users/ArthurBrigueli/repos`
+    const res = await fetch(apiGithub)
+    const data = await res.json()
+    return data
+}
+
+
+const mais_projeto = async()=>{
+    const data = await getRepositorio()
+
+    data.map(item =>{
+        if(item.name == 'pokedex-js' || item.name == 'Interactive-card-Responsive' || item.name == 'temperature-of-your-city-js' || item.name == 'Portifolio' || item.name == 'ArthurBrigueli'){
+            return
+        }else{
+            const projeto = document.createElement('div')
+            projeto.className = 'projeto'
+            projeto.innerHTML += `
+                <div class="container-img">
+                    <img class="img-git" src="../img_projeto/GitHub-Mark.png" alt="">
+                    <span class="nome-projeto">${item.name}</span>
+                </div>
+                <div class="container-info">
+                    <span class="linguagem-projeto">${item.language}</span>
+                    <a class="botao-projeto" href="${item.clone_url}" target="_blank">GitHub</a>
+                </div>
+            `
+            container_conteudo.appendChild(projeto)
+        }
+    })
+
+}
+
+
+
+
+
+mais.addEventListener('click', ()=>{
+    mais_projeto()
+    mais.remove()
+})
