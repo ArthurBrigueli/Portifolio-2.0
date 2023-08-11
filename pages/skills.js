c6246b4581a6942f302a1cb117a538eda534c163
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-08-10 23:41:05 -0300

    melhoria no codigo

--- pages/skills.js
+++ pages/skills.js
@@ -13,48 +13,52 @@ const info = [
         cor: "orange"
     },
     {
-        id: "1",
+        id: 1,
         titulo: "Css",
         desc: "Curso realizado de Css no site Curso em video, +/- 1 ano de experiencia com a tecnologia",
         cor: "rgb(157, 252, 255)"
     },
     {
-        id: "2",
+        id: 2,
         titulo: "JavaScript",
         desc: "Curso realizado de JavaScript no site Curso em video, +/- 1 ano de experiencia com a tecnologia",
         cor: "rgb(255, 255, 78)"
     },
     {
-        id: "3",
+        id: 3,
         titulo: "Csharp",
         desc: "Cursando a tecnologia Csharp no atual terceiro semestre na faculdade de analise e desenvolvimento de sistemas",
         cor: "rgb(255, 101, 255)"
     },
     {
-        id: "4",
+        id: 4,
         titulo: "Oracle",
         desc: "Cursando a tecnologia Oracle SQL no atual terceiro semestre na faculdade de analise e desenvolvimento de sistemas",
         cor: "orange"
     },
     {
-        id: "5",
+        id: 5,
         titulo: "React",
         desc: "Cursando a tecnologia React Js na Udemy - React do Zero a Maestria (c/ hooks, router, API, Projetos",
         cor: "rgb(157, 252, 255)"
     },
     {
-        id: '6',
+        id: 6,
         titulo: ' NodeJS',
-        desc: 'feito pelo canal ciencia da computação, usando todas as bibliotecas nescessaria para a criação de um projeto back e front ed',
+        desc: 'feito pelo canal ciencia da computação, usando todas as bibliotecas nescessaria para a criação de um projeto back e front',
         cor: 'green'
     }
 ]
 
 
-const mostrarDesc = (s)=>{
-    t.innerHTML = info[s].titulo
-    d.innerHTML = info[s].desc
-    t.style.color = info[s].cor
+const mostrarDesc = (sd, s)=>{
+    for(i in info){
+        if(s == info[i].id){
+            t.textContent = info[s].titulo
+            d.textContent = info[s].desc
+            t.style.color = info[s].cor
+        }
+    }
 }
 
 
@@ -66,7 +70,7 @@ const esconderDesc = () => {
 
 for(let skill = 0; skill < skills.length; skill++){
     skills[skill].addEventListener('mouseenter', ()=>{
-        mostrarDesc(skill)
+        mostrarDesc(skills[skill], skill)
     })
 
     skills[skill].addEventListener("mouseleave", ()=>{
